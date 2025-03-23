7180a370dc769ba9536161312212df352c23e710
tobeyligaya <tobeyligaya2@csus.edu>
2025-03-23 15:29:45 -0700

    SCRUM #217 - Design Sign-Up Page UI
    Made the UI look more like the Figma design

--- precision-auto/src/app/signup/page.js
+++ precision-auto/src/app/signup/page.js
@@ -1,45 +1,74 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Nav from "../constants/nav";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-        <main className={styles.main}>
-            <div className={styles.signup}>
-                <h1 className = {styles.header}>Create Your Account <img className = {styles.testing} src="/images/logo.jpg" alt="logo pic"></img></h1>
-                <h4 className = {styles.header}>to continue your experience</h4>
+    <div className={styles.signupPage}>
+        <div className={styles.leftContainer}>
+            <div className={styles.container}>
+                <h1 className={styles.header1}>Create your Account</h1>
+                <h4 className={styles.header2}>to continue your experience</h4>
 
+                <div className={styles.formContainer}>
+                    <div className={styles.inputWrapper}>
+                        <label htmlFor="first name"><b>First Name</b></label>
+                        <input className = {styles.inputbox} type="text" placeholder="Enter First Name" required></input>
+                    </div>
 
-                <label htmlFor="email"><b>Email</b></label>
-                <input className = {styles.inputtest} type="text" placeholder="Enter Email" name="email" required></input>
+                    <div className={styles.inputWrapper}>
+                        <label htmlFor="last name"><b>Last Name</b></label>
+                        <input className = {styles.inputbox} type="text" placeholder="Enter Last Name" required></input>
+                    </div>
+                </div>
 
-                <label htmlFor="first name"><b>First Name</b></label>
-                <input className = {styles.inputtest} type="text" placeholder="Enter First Name" required></input>
+                    <label htmlFor="email"><b>Email</b></label>
+                    <input className = {styles.inputbox} type="text" placeholder="Enter Email" name="email" required></input>
+                <div className={styles.formContainer}>
+                    <div className={styles.inputWrapper}>
+                        <label htmlFor="password"><b>Password</b></label>
+                        <input className = {styles.inputbox} type="text" placeholder="Enter Password" required></input>
+                    </div>
 
-                <label htmlFor="last name"><b>Last Name</b></label>
-                <input className = {styles.inputtest} type="text" placeholder="Enter Last Name" required></input>
+                    <div className={styles.inputWrapper}>
+                        <label htmlFor="confirm password"><b>Confirm Password</b></label>
+                        <input className = {styles.inputbox} type="text" placeholder="Confirm Password" required></input>
+                    </div>
+                </div>
 
-                <label htmlFor="last name"><b>Password</b></label>
-                <input className = {styles.inputtest} type="text" placeholder="Enter Password" required></input>
-                
-                <label htmlFor="last name"><b>Confirm Password</b></label>
-                <input className = {styles.inputtest} type="text" placeholder="Confirm Password" required></input>
-
-                <label htmlFor="last name"><b>Phone Number</b></label>
-                <input className = {styles.inputtest} type="text" placeholder="Enter Phone Number" required></input>
-
-                <a href="../">
-                    <button className={styles.button}>Sign Up</button>
-                </a>
-
-                <a href="../">
-                    <button className={styles.google_button}>Sign in with Google</button>
-                </a>
-
-                
-
+                    <label htmlFor="phone number"><b>Phone Number</b></label>
+                    <input className = {styles.inputbox} type="text" placeholder="Enter Phone Number" required></input>
             </div>
-        </main>
+            <a href="../">
+                <button className={styles.button + ' ' + styles.signup_button}>Sign Up</button>
+            </a>
+
+            <a href="../">
+                <button className={styles.button + ' ' + styles.google_button}>Sign in with Google
+                    <img
+                        src="/images/signup/google-logo.jpg"
+                        alt="google logo"
+                        className={styles.googlelogo}
+                        ></img>
+                        </button>
+            </a>
+                <div className={styles.loginContainer}>
+                <p>Have an account already?</p>
+                <a href="../">
+                    <p className={styles.login}>Log in</p>
+                </a>
+            </div>
+        </div>
+        <div className={styles.rightContainer}>
+            <img 
+                src = "/images/signup/b&w_logo.jpg"
+                alt = "logo pic"
+                className ={styles.logo}
+                ></img>
+            <p className={styles.description}>Subscribe to our membership and have 
+                instant access to exclusive rewards, and savings!</p>
+        </div>
     </div>
+    
   );
 }
