43f45b57b02ff5f7140abb2a2fedece299aebdca
Tobey Ligaya <tobeyligaya2@csus.edu>
2025-09-09 18:08:54 -0700

    SCRUM#163 & 165
    Designed Service Dashboard UI by adding the side bar, and adding info like Vehicle Info, Design Consideration, and Additional Details. Also added the Status Overview UI which is just a progress bar showing the status of the vehicle.

--- precision-auto/src/app/client-vehicle-status/page.js
+++ precision-auto/src/app/client-vehicle-status/page.js
@@ -29,7 +29,7 @@ export default function Home() {
             </a>
           </div>
           <div>
-            <a href="./client-portal-profile" className={styles.inactiveTab}>
+            <a href="./client-vehicle-status" className={styles.inactiveTab}>
               Past Records
             </a>
           </div>
@@ -44,28 +44,47 @@ export default function Home() {
           <div className={styles.bigContainer}>
             <div className={styles.topContainer}>
               <div className={styles.topLeftContainer}>
-                <p className={styles.profileHeader}>Profile</p>
-              </div>
-              <div className={styles.topRightContainer}>
-                <button className={styles.editButton}>Edit</button>
+                <p className={styles.profileHeader}>Vehicle Status</p>
               </div>
             </div>
             <div className={styles.smallContainer}>
               <div className={styles.inputWrapper}>
-                <p>Name:</p>
-                <p className={styles.info}>Tobey Maguire</p>
+                <p>Status Overview:</p>
+                <div className={styles.statusOverview}>
+                  <div className={styles.inputWrapper2}>
+                  <span className={styles.circle}></span>
+                  <p>In Service</p>
+                </div>
+                  <p className={styles.line}></p>
+                <div className={styles.inputWrapper2}>
+                  <span className={styles.circle}></span>
+                  <p>Repairing</p>
+                </div>
+                  <p className={styles.line}></p>
+                <div className={styles.inputWrapper2}>
+                  <span className={styles.circle}></span>
+                  <p>Ready for</p>
+                  <p>Pickup</p>
+                </div>
+                </div>
               </div>
               <div className={styles.inputWrapper}>
-                <p>Phone Number:</p>
-                <p className={styles.info}>(012) 345-6789</p>
+                <p>Vehicle Info:</p>
+                <p className={styles.info}>Make: Toyota</p>
+                <p className={styles.info}>Model: Camry</p>
+                <p className={styles.info}>Year: 2012</p>
+                <p className={styles.info}>VIN: 12345678</p>
               </div>
               <div className={styles.inputWrapper}>
-                <p>Email:</p>
-                <p className={styles.info}>example@gmail.com</p>
+                <p>Design Consideration:</p>
+                <p className={styles.info}>Color: Red</p>
+                <p className={styles.info}>Design: Yes</p>
               </div>
               <div className={styles.inputWrapper}>
-                <p>User Since:</p>
-                <p className={styles.info}>01/01/0001</p>
+                <p>Additional Details:</p>
+                <p className={styles.info}>notes from technician</p>
+                <p className={styles.info}>upgrades</p>
+                <p className={styles.info}>...</p>
               </div>
             </div>
           </div>
