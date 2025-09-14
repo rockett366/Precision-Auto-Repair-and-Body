8e51e85a66822072338292f429136b06fc4a98ee
Spectator <daryl812@gmail.com>
2025-09-14 11:38:42 -0700

    Scrum-87
    Adds TOS page and tos.html which is displayed on said page.
    Scrum-360: Page
    Scrum-361: tos.html scrollbox
    Scrum-362: checkbox
    Scrum-363: button functionality

---
+++ precision-auto/src/app/tos/page.js
@@ -0,0 +1,65 @@
+// src/app/tos/page.js
+"use client";
+
+import styles from "./page.module.css";
+import Nav from "../constants/nav.js";
+import { useState } from "react";
+
+export default function TOS() {
+
+	const [isChecked, setIsChecked] = useState(false);
+	
+	// When the checkbox is active, pressing the 'continue' button will call this function.
+	function continueClicked() {
+      console.log('Mm yes button');
+    }
+
+	const GoBack = () => {
+		history.back()
+	};
+
+	const handleChange = (event) => {
+		setIsChecked(event.target.checked);
+	};
+	return (
+		<div className={styles.wholePage}>
+			<Nav />
+			<div className={styles.container}>
+				<h1 className={styles.header}>
+					<span className={styles.line}></span>
+					Terms Of Service
+					<span className={styles.line}></span>
+				</h1>
+				<div className={styles.TOSframe}>
+				<embed className={styles.fill} type="text/html" src="tos.html"></embed>
+				</div>
+				
+				<table className={styles.table}>
+					<tbody>
+						<tr>
+							<th className={styles.buttontable}>
+								<button onClick={GoBack} className={styles.button}>
+								Go Back
+								</button>
+							</th>
+							<th className={styles.texttable}>
+								<span className={styles.agreetext}>I have read and agree to the Terms of Service</span>
+							</th>
+							<th>
+								<input type="checkbox" className={styles.checkbox}
+								checked={isChecked}
+								onChange={handleChange} />
+							</th>
+							<th className={styles.buttontable}>
+								<button disabled={!isChecked} onClick={continueClicked} className={styles.button}>
+								Continue
+								</button>
+							</th>
+						</tr>
+					</tbody>
+				</table>
+				
+			</div>
+		</div>
+	);
+}
