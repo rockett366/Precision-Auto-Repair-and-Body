17788c81678d14fc03e94e661f06051812c6c245
Tobey Ligaya <tobeyligaya2@csus.edu>
2025-09-28 20:08:34 -0700

    SCRUM #194
    Records table now implemented with the backend. Will pull data from the backend and display it on the records table. Two methods to get the data, first option using mock API for testing and second option using local db. Comment out method you don't want to use

--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel, EmailStr, Field, field_validator, model_validator
+from datetime import date
 import phonenumbers
 from typing import Optional
 
@@ -78,3 +79,11 @@ class ReviewOut(BaseModel):
 
     class Config:
         from_attributes = True
+
+class ClientRecordBase(BaseModel):
+    vehicle: str
+    description: str
+    date: date
+    
+    class Config:
+        orm_mode = True
