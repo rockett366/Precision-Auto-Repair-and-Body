2ff8d7d62136799a6f4d633b56c2f3412e5fcb93
Tawxyn <oleshchukmike@gmail.com>
2025-09-30 18:17:11 -0700

    coverage support

--- precision-auto/jest.config.cjs
+++ precision-auto/jest.config.cjs
@@ -12,9 +12,19 @@ module.exports = createJestConfig({
     '<rootDir>/src/**/__tests__/**/*.(spec|test).{js,jsx}',
     '<rootDir>/tests/integration/**/*.(spec|test).{js,jsx}',
   ],
+
+  // coverage settings
   collectCoverageFrom: [
     'src/**/*.{js,jsx}',
     '!src/**/_app.*',
     '!src/**/_document.*',
+    '!src/**/__tests__/**',
+    '!src/test-utils/**',
   ],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'text-summary', 'html', 'lcov'],
+
+  coverageThreshold: {
+    global: { branches: 70, functions: 75, lines: 80, statements: 80 }
+  },
 });
